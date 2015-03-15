18a3fbb024fe7b2060b1686bf6d8d4e63ca25f86
Jiro Nagashima <hedjirog@gmail.com>
2015-03-15 11:18:06 +0900

    Add project files for intro animaiton

---
+++ Intro/Intro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Intro
+//
+//  Created by Jiro Nagashima on 3/15/15.
+//  Copyright (c) 2015 Jiro Nagashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
